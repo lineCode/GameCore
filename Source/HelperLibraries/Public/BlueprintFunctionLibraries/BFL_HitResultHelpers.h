c991af012d987fd0ad4892a7fa050522a9ee1ed7
Brian2524 <b2hinkle@gmail.com>
2022-04-28 01:39:43 -0400

    remove unused, unnecissary functions

--- Source/HelperLibraries/Public/BlueprintFunctionLibraries/BFL_HitResultHelpers.h
+++ Source/HelperLibraries/Public/BlueprintFunctionLibraries/BFL_HitResultHelpers.h
@@ -19,16 +19,7 @@ class HELPERLIBRARIES_API UBFL_HitResultHelpers : public UBlueprintFunctionLibra
 	GENERATED_BODY()
 	
 public:
-	/** Returns the material that was hit from the trace (requires passed in Hit Result to have a face index) */
-	UFUNCTION(BlueprintPure, Category = "HitResultHelpers")
-		static UMaterialInterface* GetHitMaterial(const FHitResult& InHitResult);
-
-	/** Returns the section index of the Primitive Component that was hit from the trace */
-	UFUNCTION(BlueprintPure, Category = "HitResultHelpers|HitInfo")
-		static void GetSectionLevelHitInfo(const FHitResult& InHitResult, UPrimitiveComponent*& OutHitPrimitiveComponent, int32& OutHitSectionIndex);
-
 	/** Returns true if the two given hits were from the same trace */
 	UFUNCTION(BlueprintPure, Category = "HitResultHelpers")
 		static bool AreHitsFromSameTrace(const FHitResult& HitA, const FHitResult& HitB);
-
 };
