5d91cf07530d229cf81c3e6da2df723b8cb6d194
Brian2524 <b2hinkle@gmail.com>
2022-07-19 11:46:55 -0500

    Create base AssetManager class

---
+++ Source/GameCore/Public/GCAssetManager.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/AssetManager.h"
+#include "GCAssetManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMECORE_API UGCAssetManager : public UAssetManager
+{
+	GENERATED_BODY()
+	
+};
