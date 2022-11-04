33acda1a911f541596db72c2ec7e5196d377f26b
Brian2524 <b2hinkle@gmail.com>
2022-11-04 10:28:00 -0400

    Fix old references to FGCFloatPropertyWrapper in generic code.
    That's embarassing.
    
    Co-authored-by: Christian Hinkle <c4hinkle@gmail.com>

--- Source/GameCore/Public/Types/PropertyWrappers/GCPropertyWrapper.h
+++ Source/GameCore/Public/Types/PropertyWrappers/GCPropertyWrapper.h
@@ -35,7 +35,7 @@ GC_PROPERTY_WRAPPER_TYPE(ValueTypeName)()\
 }\
 \
 GC_PROPERTY_WRAPPER_TYPE(ValueTypeName)(UObject* InPropertyOwner, const FName& PropertyName, const ValueType& InValue = DefaultValue)\
-	: FGCFloatPropertyWrapper()\
+	: GC_PROPERTY_WRAPPER_TYPE(ValueTypeName)()\
 {\
 	PropertyOwner = InPropertyOwner;\
 	Value = InValue;\
@@ -44,7 +44,7 @@ GC_PROPERTY_WRAPPER_TYPE(ValueTypeName)(UObject* InPropertyOwner, const FName& P
 	/* Get the FProperty so we can use push model with it */\
 	Property = FindFProperty<FProperty>(PropertyOwner->GetClass(), PropertyName);\
 	\
-	/* Ensure this property exists on the owner and that this property is an FGCFloatPropertyWrapper! */\
+	/* Ensure this property exists on the owner and that this property is an GC_PROPERTY_WRAPPER_TYPE(ValueTypeName)! */\
 	if constexpr (!(UE_BUILD_SHIPPING || UE_BUILD_TEST))\
 	{\
 		if (!Property.Get())\
@@ -52,9 +52,9 @@ GC_PROPERTY_WRAPPER_TYPE(ValueTypeName)(UObject* InPropertyOwner, const FName& P
 			UE_LOG(LogGCPropertyWrapper, Error, TEXT("%s(): The given PropertyName \"%s\" was not found on the PropertyOwner ``%s``. Ensure correct spelling for the property you are looking for and make sure that it is a UPROPERTY so we can find it!"), ANSI_TO_TCHAR(__FUNCTION__), *(PropertyName.ToString()), *(InPropertyOwner->GetName()));\
 			check(0);\
 		}\
-		else if (!(CastField<FStructProperty>(Property.Get()) && CastField<FStructProperty>(Property.Get())->Struct == FGCFloatPropertyWrapper::StaticStruct()))\
+		else if (!(CastField<FStructProperty>(Property.Get()) && CastField<FStructProperty>(Property.Get())->Struct == GC_PROPERTY_WRAPPER_TYPE(ValueTypeName)::StaticStruct()))\
 		{\
-			UE_LOG(LogGCPropertyWrapper, Error, TEXT("%s(): The given FProperty ``%s::%s`` is not a(n) %s!"), ANSI_TO_TCHAR(__FUNCTION__), *(InPropertyOwner->GetClass()->GetName()), *(Property->GetFName().ToString()), *(FGCFloatPropertyWrapper::StaticStruct()->GetName()));\
+			UE_LOG(LogGCPropertyWrapper, Error, TEXT("%s(): The given FProperty ``%s::%s`` is not a(n) %s!"), ANSI_TO_TCHAR(__FUNCTION__), *(InPropertyOwner->GetClass()->GetName()), *(Property->GetFName().ToString()), *(GC_PROPERTY_WRAPPER_TYPE(ValueTypeName)::StaticStruct()->GetName()));\
 			check(0);\
 		}\
 	}\
@@ -62,7 +62,7 @@ GC_PROPERTY_WRAPPER_TYPE(ValueTypeName)(UObject* InPropertyOwner, const FName& P
 \
 virtual ~GC_PROPERTY_WRAPPER_TYPE(ValueTypeName)() { }\
 \
-/** An easy conversion from this struct to ValueType. This allows FGCFloatPropertyWrapper to be treated as a ValueType in code */\
+/** An easy conversion from this struct to ValueType. This allows GC_PROPERTY_WRAPPER_TYPE(ValueTypeName) to be treated as a ValueType in code */\
 operator ValueType() const\
 {\
 	return Value;\
