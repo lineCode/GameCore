68684490ef8126799873468eb77386032bc4e348
Brian2524 <b2hinkle@gmail.com>
2022-11-06 18:49:33 -0500

    Move GCFloatPropertyWrapper to new general GCPropertyWrappers.h
    Also make a GCInt32PropertyWrapper.
    
    Co-authored-by: Christian Hinkle <c4hinkle@gmail.com>

--- Source/GameCore/Public/Types/PropertyWrappers/GCPropertyWrappers.h
+++ Source/GameCore/Public/Types/PropertyWrappers/GCPropertyWrappers.h
@@ -5,11 +5,12 @@
 #include "CoreMinimal.h"
 #include "GCPropertyWrapperBase.h"
 
-#include "GCFloatPropertyWrapper.generated.h"
+#include "GCPropertyWrappers.generated.h"
 
 
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FGCFloatValueChange, const float&, OldValue, const float&, NewValue); // make the required delegate type for this property wrapper
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FGCInt32ValueChange, const int32&, OldValue, const int32&, NewValue);
 
 
 USTRUCT()
@@ -32,5 +33,29 @@ struct TStructOpsTypeTraits<FGCFloatPropertyWrapper> : public TStructOpsTypeTrai
 	{
 		WithSerializer = true, // required for the property wrapper
 		WithNetSerializer = true // required for the property wrapper
+	};
+};
+
+
+
+USTRUCT()
+struct GAMECORE_API FGCInt32PropertyWrapper : public FGCPropertyWrapperBase
+{
+	GENERATED_BODY()
+
+	GC_PROPERTY_WRAPPER_MEMBERS(int32, Int32, 0.f);
+
+private:
+	UPROPERTY(EditAnywhere)
+		int32 Value;
+};
+
+template<>
+struct TStructOpsTypeTraits<FGCInt32PropertyWrapper> : public TStructOpsTypeTraitsBase2<FGCInt32PropertyWrapper>
+{
+	enum
+	{
+		WithSerializer = true,
+		WithNetSerializer = true
 	};
 };
